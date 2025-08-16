82150fb316934027f6575bc7966d599fa0171d6c
Imjaopedro <joaovale.claudio10@gmail.com>
2025-08-16 16:16:22 -0300

    created table configuration of tasks model and new route with gorrila mux

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"github.com/Imjaopedro/firstGoProject/config"
+	"github.com/Imjaopedro/firstGoProject/models"
+	"github.com/gorilla/mux"
 )
 
 // Function called when program start
@@ -13,8 +15,21 @@ import (
 func main() {
 
 	dbConnection := config.SetUpDatabase()
-
 	defer dbConnection.Close()
+
+	_, err := dbConnection.Exec(models.CreateTableSQL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/tasks", func(w http.ResponseWriter,
+		r *http.Request) {
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, World!"))
+	}).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":3030", nil))
 
